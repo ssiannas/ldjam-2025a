c92cabf1da176656ba596848bf7aea79de43e822
SpyrosSiannas <ssiannas@gmail.com>
2025-04-05 23:38:11 +0300

    Update terrain prefab scales

---
+++ Assets/FloaterController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace ldjam_hellevator
+{
+    public class FloaterController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
